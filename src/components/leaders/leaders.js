85b47061c4a475086cc8ffda6539b79f2a82cdf0
uscracken <colfarrell@yandex.ru>
2021-08-24 00:40:59 +0300

    rewrite correctly in sass way leader styles before items

--- src/components/leaders/leaders.js
+++ src/components/leaders/leaders.js
@@ -23,9 +23,9 @@ export default class Leaders extends Component {
 
     render() {
         return (
-            <article id="leaders" className="theme_light" onClick={this.toggleTheme}>
-                <h1 id="leaders_h1">Больше всего ️коммитов</h1>
-                <p id="leaders_p">Спринт № 213</p> 
+            <article className="leaders theme_light" onClick={this.toggleTheme}>
+                <h1 className="leaders_h1">Больше всего ️коммитов</h1>
+                <p className="leaders_p">Спринт № 213</p> 
 
                 <div className="winners leaders_light">
                     <div id="item5" className="winner_item">
@@ -33,7 +33,7 @@ export default class Leaders extends Component {
                             <div className="emodji">👍</div>
                             <div id="pic5" className="pic"></div>
                             <p>Александр Иванков</p>
-                            <div id="age">19</div>
+                            <div className="age">19</div>
                         </div>
                         <div className="win_place place_5">
                             <div id="num5">5</div> 
@@ -43,7 +43,7 @@ export default class Leaders extends Component {
                         <div className="wrapper">
                             <div id="pic3" className="pic"></div>
                             <p>Дмитрий Андриянов</p>
-                            <div id="age">22</div>
+                            <div className="age">22</div>
                         </div>
                         <div className="win_place place_3">
                             <div id="num3">3</div>
@@ -54,7 +54,7 @@ export default class Leaders extends Component {
                             <div className="emodji">👑</div>
                             <div id="pic1" className="pic"></div>
                             <p>Дарья Ковалева</p>
-                            <div id="age">32</div>
+                            <div className="age">32</div>
                         </div>
                         <div className="win_place place_1">
                             <div id="num1">1</div>
@@ -64,7 +64,7 @@ export default class Leaders extends Component {
                         <div className="wrapper">
                             <div id="pic2" className="pic"></div>
                             <p>Сергей Бережной</p>
-                            <div id="age">27</div>
+                            <div className="age">27</div>
                         </div>
                         <div className="win_place place_2">
                             <div id="num2">2</div>
@@ -74,7 +74,7 @@ export default class Leaders extends Component {
                         <div className="wrapper">
                             <div id="pic4" className="pic"></div>
                             <p>Андрей Мокроусов</p>
-                            <div id="age">20</div>
+                            <div className="age">20</div>
                         </div>
                         <div className="win_place place_4">
                             <div id="num4">4</div>
